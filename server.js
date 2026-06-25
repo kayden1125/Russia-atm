5dd2980d8528c4d4e1cb8ffa3d4f53eea51b1f25
kayden1125 <151122644+kayden1125@users.noreply.github.com>
2026-06-25 10:36:32 +0400

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ server.listen(PORT, () => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('ATM System running on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`ATM System running on port ${PORT}`);
 });
+
