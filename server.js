c917f5d9f4cada5042376f35f31e6f091240f1af
kayden1125 <151122644+kayden1125@users.noreply.github.com>
2026-06-25 10:53:42 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
 const app = express();
+const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(server);
 
-// This tells the server to load your HTML files
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/atm.html');
 });
@@ -15,16 +13,15 @@ app.get('/operator', (req, res) => {
     res.sendFile(__dirname + '/operator.html');
 });
 
-// This handles the instant messaging between devices
 io.on('connection', (socket) => {
     console.log(`[SYSTEM] Device connected: ${socket.id}`);
 
     socket.on('atm_event', (data) => {
-        io.emit('operator_update', data); 
+        io.emit('operator_update', data);
     });
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`ATM System running on port ${PORT}`);
-});  //
-
+});
